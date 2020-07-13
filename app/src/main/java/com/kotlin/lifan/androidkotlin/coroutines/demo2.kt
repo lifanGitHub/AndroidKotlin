85e6f520e08589cc9eca19738870d005f6b02959
lifan <494360059@qq.com>
2020-07-13 15:05:47 +0800

    协程demo

---
+++ app/src/main/java/com/kotlin/lifan/androidkotlin/coroutines/demo2.kt
@@ -0,0 +1,42 @@
+package com.kotlin.lifan.androidkotlin.coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Author: LiFan
+ * Date: 2020/7/13
+ * Description:
+ */
+
+fun main() = runBlocking {
+    launch { doWorld2() }
+    launch { doWorld() }
+    println("Hello,")
+}
+
+//等效
+//fun main() {
+//    GlobalScope.launch {
+//        launch { doWorld2() }
+//        launch { doWorld() }
+//        println("Hello,")
+//    }
+//
+//
+//    while (true){}
+//}
+
+
+// 这是你的第一个挂起函数
+suspend fun doWorld() {
+    delay(1000L)
+    println("World!")
+}
+
+suspend fun doWorld2() {
+    delay(1100L)
+    print("!!")
+}
